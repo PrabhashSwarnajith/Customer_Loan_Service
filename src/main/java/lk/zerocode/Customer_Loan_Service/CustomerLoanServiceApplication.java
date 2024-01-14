f928e8750ac92a8dd7d55b3052afb9bcf219f4fd
PrabhashSwarnajith <prabashswarnajith2001@gmail.com>
2024-01-14 23:59:34 +0530

    Add spring project

---
+++ src/main/java/lk/zerocode/Customer_Loan_Service/CustomerLoanServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.zerocode.Customer_Loan_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerLoanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerLoanServiceApplication.class, args);
+	}
+
+}
